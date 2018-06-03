06c6ece975b1ba5551841570624d16a49c40bee7
heldcoindev <heldcoindev>
2018-06-03 15:25:51 +0300

    fix udpate

--- src/main.cpp
+++ src/main.cpp
@@ -2533,27 +2533,76 @@ bool CBlock::CheckBlock(bool fCheckPOW, bool fCheckMerkleRoot, bool fCheckSig) c
                     bool foundPaymentAndPayee = false;
 
                     CScript payee;
+					string targetNode;
                     CTxIn vin;
-                    if(!masternodePayments.GetBlockPayee(pindexBest->nHeight+1, payee, vin) || payee == CScript()){
-                        foundPayee = true; //doesn't require a specific payee
-                        foundPaymentAmount = true;
-                        foundPaymentAndPayee = true;
-                        if(fDebug) { LogPrintf("CheckBlock() : Using non-specific masternode payments %d\n", pindexBest->nHeight+1); }
+					
+					CScript payeerewardaddress = CScript();
+					int payeerewardpercent = 0;
+					bool hasPayment = true;
+
+					if(!masternodePayments.GetBlockPayee(pindexBest->nHeight+1, payee, vin)){
+						CMasternode* winningNode = mnodeman.GetCurrentMasterNode(1);
+						if(winningNode){
+							payee = GetScriptForDestination(winningNode->pubkey.GetID());
+							payeerewardaddress = winningNode->rewardAddress;
+							payeerewardpercent = winningNode->rewardPercentage;
+					   
+						if(hasPayment && payeerewardpercent == 0){
+							CTxDestination address1;
+							ExtractDestination(payee, address1);
+							CHeldCoinAddress address2(address1);
+							targetNode = address2.ToString().c_str();	
+						}
+
+						if(hasPayment && payeerewardpercent == 100){
+						CTxDestination address1;
+							ExtractDestination(payeerewardaddress, address1);
+							CHeldCoinAddress address2(address1);
+							targetNode = address2.ToString().c_str();
+							
+						}
+
+						if(hasPayment && payeerewardpercent > 0 && payeerewardpercent < 100){
+							CTxDestination address1;
+							ExtractDestination(payee, address1);
+							CHeldCoinAddress address2(address1);
+							
+							CTxDestination address3;
+							ExtractDestination(payeerewardaddress, address3);
+							CHeldCoinAddress address4(address3);
+							targetNode = address2.ToString().c_str();
+							
+						}
+						LogPrintf("Detected Masternode payment to %s\n", targetNode);	
+						} else {
+							LogPrintf("Cant calculate Winner, so passing.");                        
+                            foundPaymentAmount = true;
+                            foundPayee = true;
+                            foundPaymentAndPayee = true;
+                        }
                     }
 
+					
                     for (unsigned int i = 0; i < vtx[1].vout.size(); i++) {
+						CTxDestination address1;
+						ExtractDestination(vtx[1].vout[i].scriptPubKey, address1);
+						CHeldCoinAddress address2(address1);   
                         if(vtx[1].vout[i].nValue == masternodePaymentAmount )
                             foundPaymentAmount = true;
-                        if(vtx[1].vout[i].scriptPubKey == payee )
+                        if(address2.ToString().c_str() == targetNode)
                             foundPayee = true;
-                        if(vtx[1].vout[i].nValue == masternodePaymentAmount && vtx[1].vout[i].scriptPubKey == payee)
+						if(vtx[1].vout[i].nValue == masternodePaymentAmount && address2.ToString().c_str() == targetNode)
                             foundPaymentAndPayee = true;
                     }
 
                     CTxDestination address1;
                     ExtractDestination(payee, address1);
                     CHeldCoinAddress address2(address1);
-
+					if (pindexBest->nHeight+1 < 250000) { // TODO: remove magic number; use in one place
+						foundPaymentAmount = true;
+						foundPayee = true;
+						foundPaymentAndPayee = true;
+					}
                     if(!foundPaymentAndPayee) {
                         if(fDebug) { LogPrintf("CheckBlock() : Couldn't find masternode payment(%d|%d) or payee(%d|%s) nHeight %d. \n", foundPaymentAmount, masternodePaymentAmount, foundPayee, address2.ToString().c_str(), pindexBest->nHeight+1); }
                         return DoS(100, error("CheckBlock() : Couldn't find masternode payment or payee"));
@@ -3549,11 +3598,11 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
         CAddress addrFrom;
         uint64_t nNonce = 1;
         vRecv >> pfrom->nVersion >> pfrom->nServices >> nTime >> addrMe;
-        if (pfrom->nVersion < MIN_PEER_PROTO_VERSION)
+        if (nBestHeight >= 70000 && pfrom->nVersion < MIN_PEER_PROTO_VERSION)
         {
             // disconnect from peers older than this proto version
             LogPrintf("partner %s using obsolete version %i; disconnecting\n", pfrom->addr.ToString(), pfrom->nVersion);
-            pfrom->fDisconnect = true;
+             pfrom->fDisconnect = true; 
             return false;
         }
 
